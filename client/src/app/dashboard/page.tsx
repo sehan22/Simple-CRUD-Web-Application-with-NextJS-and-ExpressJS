c472dd7080fe2c06ec3c076e17264e6866b489b5
sehan22 <sehanranaweera2002@gmail.com>
2024-02-08 09:00:13 +0530

    dashboard Colour Pallets card UI updated.

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FaCalendar, FaClipboardList, FaComments, FaDollarSign, FaDownload} from "react-icons/fa";
+import {FaCalendar, FaClipboardList, FaComments, FaDollarSign, FaDownload, FaLongArrowAltRight} from "react-icons/fa";
 
 export default function Dashboard() {
     return (
@@ -87,78 +87,84 @@ export default function Dashboard() {
             </div>
 
             {/*details*/}
-            <div className="flex w-full justify-center items-center mx-auto">
+            <div className="flex w-full justify-center items-start mx-auto gap-5">
 
                 <div className="flex flex-col justify-center items-center gap-5 flex-1">
 
                     {/*projects*/}
-                    <div className="w-full bg-Light rounded">
+                    <div className="w-full bg-Light rounded" style={{boxShadow: '0 0px 24px rgba(0, 0, 0, 0.08)'}}>
 
-                        <div className="flex justify-between items-center p-5 border-b-2 py-3 font-nunito text-subtopic text-Primary font-bold">
+                        <div
+                            className="flex justify-between items-center p-5 border-b-2 border-gray-200 border-opacity-50 py-3 font-nunito text-subtopic text-Primary font-bold">
                             Project
                         </div>
 
                         {/*Server Migration*/}
-                        <div className="flex flex-col justify-center p-5 items-center w-full bg-white font-nunito text-[13px] text-Secondary font-bold">
+                        <div
+                            className="flex flex-col justify-center p-5 items-center w-full bg-white font-nunito text-[13px] text-Secondary font-bold">
 
                             <div className="flex justify-between items-center w-full">
                                 <h1>Server Migration</h1>
                                 <h1>20%</h1>
                             </div>
 
-                            <div className="w-full h-4 bg-Secondary bg-opacity-20 rounded-full">
+                            <div className="w-full h-4 bg-Secondary bg-opacity-20 rounded-md">
                                 <div className="w-[20%] rounded-full h-full bg-Danger"></div>
                             </div>
                         </div>
 
-                        {/*Server Migration*/}
-                        <div className="flex flex-col justify-center p-5 items-center w-full bg-white font-nunito text-[13px] text-Secondary font-bold">
+                        {/*Sales Tracking*/}
+                        <div
+                            className="flex flex-col justify-center p-5 items-center w-full bg-white font-nunito text-[13px] text-Secondary font-bold">
 
                             <div className="flex justify-between items-center w-full">
-                                <h1>Server Migration</h1>
+                                <h1>Sales Tracking</h1>
                                 <h1>40%</h1>
                             </div>
 
-                            <div className="w-full h-4 bg-Secondary bg-opacity-20 rounded-full">
+                            <div className="w-full h-4 bg-Secondary bg-opacity-20 rounded-md">
                                 <div className="w-[40%] rounded-full h-full bg-Warning"></div>
                             </div>
                         </div>
 
-                        {/*Server Migration*/}
-                        <div className="flex flex-col justify-center p-5 items-center w-full bg-white font-nunito text-[13px] text-Secondary font-bold">
+                        {/*Customer Database*/}
+                        <div
+                            className="flex flex-col justify-center p-5 items-center w-full bg-white font-nunito text-[13px] text-Secondary font-bold">
 
                             <div className="flex justify-between items-center w-full">
-                                <h1>Server Migration</h1>
+                                <h1>Customer Database</h1>
                                 <h1>60%</h1>
                             </div>
 
-                            <div className="w-full h-4 bg-Secondary bg-opacity-20 rounded-full">
+                            <div className="w-full h-4 bg-Secondary bg-opacity-20 rounded-md">
                                 <div className="w-[60%] rounded-full h-full bg-Primary"></div>
                             </div>
                         </div>
 
-                        {/*Server Migration*/}
-                        <div className="flex flex-col justify-center p-5 items-center w-full bg-white font-nunito text-[13px] text-Secondary font-bold">
+                        {/*Payout Details*/}
+                        <div
+                            className="flex flex-col justify-center p-5 items-center w-full bg-white font-nunito text-[13px] text-Secondary font-bold">
 
                             <div className="flex justify-between items-center w-full">
-                                <h1>Server Migration</h1>
+                                <h1>Payout Details</h1>
                                 <h1>80%</h1>
                             </div>
 
-                            <div className="w-full h-4 bg-Secondary bg-opacity-20 rounded-full">
+                            <div className="w-full h-4 bg-Secondary bg-opacity-20 rounded-md">
                                 <div className="w-[80%] rounded-full h-full bg-Info"></div>
                             </div>
                         </div>
 
-                        {/*Server Migration*/}
-                        <div className="flex flex-col justify-center p-5 items-center w-full bg-white font-nunito text-[13px] text-Secondary font-bold">
+                        {/*Account Setup*/}
+                        <div
+                            className="flex flex-col justify-center p-5 items-center rounded-b w-full bg-white font-nunito text-[13px] text-Secondary font-bold">
 
                             <div className="flex justify-between items-center w-full">
-                                <h1>Server Migration</h1>
+                                <h1>Account Setup</h1>
                                 <h1>Completed!</h1>
                             </div>
 
-                            <div className="w-full h-4 bg-Secondary bg-opacity-20 rounded-full">
+                            <div className="w-full h-4 bg-Secondary bg-opacity-20 rounded-md">
                                 <div className="w-[100%] rounded-full h-full bg-Success"></div>
                             </div>
                         </div>
@@ -166,17 +172,71 @@ export default function Dashboard() {
                     </div>
 
                     {/*colour pallets*/}
-                    <div>a</div>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+
+                        <div className="flex flex-col rounded justify-center items-center w-full bg-Primary p-5 font-bold">
+                            <h1>Primary</h1>
+                            <h1>#4e73df</h1>
+                        </div>
+
+                    </div>
                 </div>
 
 
                 <div className="flex flex-col justify-center items-center gap-5 flex-1">
 
                     {/*Illustrations*/}
-                    <div>a</div>
+                    <div className="w-full bg-Light rounded" style={{boxShadow: '0 0px 24px rgba(0, 0, 0, 0.08)'}}>
+                        <div
+                            className="flex justify-between items-center p-5 border-b-2 border-gray-200 border-opacity-50 py-3 font-nunito text-subtopic text-Primary font-bold">
+                            Illustrations
+                        </div>
+
+                        <div
+                            className="flex flex-col justify-center p-5 gap-5 items-center w-full bg-white">
+                            <div className="w-[400px] h-[200px] bg-gray-200"></div>
+
+                            <div className="flex flex-col justify-center items-center w-full gap-5">
+                                <p className="text-subtopic text-Secondary">
+                                    Add some quality, svg illustrations to your project courtesy of unDraw, a constantly
+                                    updated collection of beautiful svg images that you can use completely free and
+                                    without attribution!
+                                </p>
+
+                                <div className="w-full">
+                                    <h1 className="flex justify-start items-center gap-2 text-start text-Primary">
+                                        Browse Illustrations on unDraw
+                                        <FaLongArrowAltRight/>
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                     {/*Development Approach*/}
-                    <div>a</div>
+                    <div className="w-full bg-Light rounded mb-10"
+                         style={{boxShadow: '0 0px 24px rgba(0, 0, 0, 0.08)'}}>
+                        <div
+                            className="flex justify-between items-center p-5 border-b-2 border-gray-200 border-opacity-50 py-3 font-nunito text-subtopic text-Primary font-bold">
+                            Development Approach
+                        </div>
+
+                        <div
+                            className="flex flex-col justify-center p-5 items-center w-full bg-white font-nunito">
+
+                            <div className="flex flex-col gap-5 justify-between items-center w-full">
+                                <p className="text-subtopic text-Secondary">
+                                    SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce CSS
+                                    bloat and poor page performance. Custom CSS classes are used to create custom
+                                    components and custom utility classes.
+                                </p>
+
+                                <p className="text-subtopic text-Secondary">
+                                    Before working with this theme, you should become familiar with the Bootstrap framework, especially the utility classes.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
 
